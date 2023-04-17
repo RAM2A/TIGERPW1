cc07c7a984bc69927d0c68732ebda4f430158756
PratikLPP <119912597+PratikLPP@users.noreply.github.com>
2023-04-17 08:32:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "5866618774:AAE7R2mVUU2UhRNoE7ZTNnztg43nbA7h-PU"
+    API_ID = 29410389
+    API_HASH = "0c716764715886f6641477ffbb63e1ee"
+    AUTH_USERS = '6253923773'
+
